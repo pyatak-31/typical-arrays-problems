b4de0f792d65a977002900a47f52a165f3cf6032
Oleg <pirr31@mail.ru>
2021-02-22 13:57:21 +0300

    feat: implement functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (array && array.length) ? Math.min.apply(null,array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (array && array.length) ? Math.max.apply(null,array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (array && array.length) ? array.reduce((x,y) => x + y) / array.length : 0;
 }
